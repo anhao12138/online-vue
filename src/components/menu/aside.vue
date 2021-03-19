314a119ed5aa4983dfc6b266d734353d27ea81c9
anhao12138 <1993697020@qq.com>
2021-03-19 14:12:44 +0800

    master

---
+++ src/components/menu/aside.vue
@@ -0,0 +1,120 @@
+<template>
+   <div class="Aside">
+     <!-- 一级分类 -->
+    <div slot="title"  v-for="item in asideList" :key="item.id" class="rec-type">
+      <!-- 一级导航 -->
+      <span slot="title" class="rec-type-title">{{item.name}}<i class="el-icon-caret-right"></i></span>
+      <p class="rec-type-exa">{{getThree(item.child)}}</p>
+      <!-- 二级导航 -->
+      <div class="rec-type-child" >
+        <span slot="title" >{{item.name}}</span>
+        <div style="display: flex;flex-wrap: wrap">
+          <span @click="$router.push({name:'Recruitment',params:{keyWords:typeChild}})" v-for="typeChild in item.child">{{typeChild}}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {T1, T2, T3, T4, T5,T6,T7,T8} from "../../data/jobs";
+export default {
+    name:"Aside",
+    data(){
+        return{
+            asideList:[
+                T1,T2,T3,T4,T5,T6,T7,T8
+            ]
+        }
+    },
+  methods:{
+    getThree(item){
+      return `${item[0]}/${item[1]}/${item[2]}`
+    }
+  }
+}
+</script>
+
+<style>
+.Aside{
+    width: 260px;
+    height: 560px;
+    background: white;
+    box-shadow: 0 0 3px red;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+.rec-type{
+    transition: all .2s;
+  padding: 0 20px;
+  height: auto;
+  width: 100%;
+  display: flex;
+  flex-direction :column;
+  justify-content: center;
+  position: relative;
+  cursor: pointer;
+  line-height: 24px;
+}
+:hover{
+    background: rgb(253, 248, 248);
+}
+:hover .rec-type-child{
+    display: flex;
+}
+ :first-child .rec-type-child{
+     top: 0;
+ }
+ :last-child .rec-type-child{
+      top: -150%;
+ }
+ :hover.rec-type-title,.rec-type-exa{
+     color: rgb(255,92,91) ;
+ }
+.rec-type-title{
+    display: flex;
+    font-weight: 100;
+    justify-content: space-between;
+} 
+.rec-type-exa{
+    letter-spacing: 3px;
+    color: rosybrown;
+    font-size: 15px;
+    overflow: hidden;
+    white-space:nowrap;
+    text-overflow: ellipsis;
+}
+.rec-type-child{
+    transition: all .8s;
+    display: none;
+    padding: 20px;
+    width: 500px;
+    height: auto;
+    min-width: 160px;
+    background: white;
+    top: -80%;
+    position: absolute;
+    left: 100%;
+    flex-direction: column;
+    flex-wrap: wrap;
+    box-shadow: 0 0 3px rgb(255,236,236);
+    z-index: 10;
+}
+span{
+    display: block;
+    font-size: 14px;
+    margin-right: 15px;
+    color: rgb(255,92,91);
+    cursor: pointer;
+}
+span:hover{
+    color: rgb(100, 98, 98);
+}
+.el-icon-caret-right{
+    color: rgb(153,153,153);
+}
+   
+</style>
+
+
--- src/views/homepage/homepage.vue
+++ src/views/homepage/homepage.vue
@@ -13,7 +13,13 @@
   </el-header>
   <!-- 主体 -->
     <el-main>
-        
+      <home-aside></home-aside>
+  <!-- <el-carousel :interval="4000" type="card" height="200px">
+    <el-carousel-item v-for="item in 6" :key="item">
+      <h3 class="medium">{{ item }}</h3>
+    </el-carousel-item>
+  </el-carousel> -->
+
     </el-main>
     </el-container>
 </template>
@@ -24,16 +30,18 @@ import Navbar from '../layout/navbar'
 import BreadCrumb from '../layout/bread-crumb'
 import MenuDrawer from '../layout/drawer'
 import Hamburger from '../layout/hamburger'
+import HomeAside from "../../components/menu/aside";
 
 export default {
     name:'homepage',
     components:{
         Avator,
-        Hamburger,
-        MenuDrawer,
         MainArea,
         Navbar,
-        BreadCrumb,   
+        BreadCrumb,
+        MenuDrawer, 
+        Hamburger,
+        HomeAside
     },
     data () {
     return {
@@ -66,7 +74,21 @@ export default {
     margin: 0px;
     position: relative; 
 }
+ .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
 
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
 
 
 </style>
